9af9437473ac97a20819108e4db45cdadcfc410c
LekoArts <lekoarts@gmail.com>
2019-03-04 12:15:55 +0100

    use simplified promise wrapper

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,21 @@
-const path = require('path')
 const _ = require('lodash')
 
-// graphql function returns a promise so we can use this little promise helper to have a nice result/error state
+// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
 const wrapper = promise =>
-  promise
-    .then(result => {
-      if (result.errors) {
-        throw result.errors
-      }
-      return { result, error: null }
-    })
-    .catch(error => ({ error, result: null }))
+  promise.then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    return result
+  })
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const postTemplate = path.resolve('src/templates/post.jsx')
-  const categoryTemplate = path.resolve('src/templates/category.jsx')
+  const postTemplate = require.resolve('./src/templates/post.jsx')
+  const categoryTemplate = require.resolve('./src/templates/category.jsx')
 
-  const { error, result } = await wrapper(
+  const result = await wrapper(
     graphql(`
       {
         allPrismicPost {
@@ -44,43 +41,37 @@ exports.createPages = async ({ graphql, actions }) => {
     `)
   )
 
-  if (!error) {
-    const categorySet = new Set()
-    const postsList = result.data.allPrismicPost.edges
+  const categorySet = new Set()
+  const postsList = result.data.allPrismicPost.edges
 
-    // Double check that the post has a category assigned
-    postsList.forEach(edge => {
-      if (edge.node.data.categories[0].category) {
-        edge.node.data.categories.forEach(cat => {
-          categorySet.add(cat.category.document[0].data.name)
-        })
-      }
-
-      // The uid you assigned in Prismic is the slug!
-      createPage({
-        path: `/${edge.node.uid}`,
-        component: postTemplate,
-        context: {
-          // Pass the unique ID (uid) through context so the template can filter by it
-          uid: edge.node.uid,
-        },
+  // Double check that the post has a category assigned
+  postsList.forEach(edge => {
+    if (edge.node.data.categories[0].category) {
+      edge.node.data.categories.forEach(cat => {
+        categorySet.add(cat.category.document[0].data.name)
       })
+    }
+
+    // The uid you assigned in Prismic is the slug!
+    createPage({
+      path: `/${edge.node.uid}`,
+      component: postTemplate,
+      context: {
+        // Pass the unique ID (uid) through context so the template can filter by it
+        uid: edge.node.uid,
+      },
     })
+  })
 
-    const categoryList = Array.from(categorySet)
+  const categoryList = Array.from(categorySet)
 
-    categoryList.forEach(category => {
-      createPage({
-        path: `/categories/${_.kebabCase(category)}`,
-        component: categoryTemplate,
-        context: {
-          category,
-        },
-      })
+  categoryList.forEach(category => {
+    createPage({
+      path: `/categories/${_.kebabCase(category)}`,
+      component: categoryTemplate,
+      context: {
+        category,
+      },
     })
-
-    return
-  }
-
-  console.log(error)
+  })
 }
